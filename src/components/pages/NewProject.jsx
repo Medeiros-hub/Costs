1edb92aa0e848f86a4b5acb114da1d12f6fb45eb
Medeiros-hub <medeiroshenriqueramalhopedro@gmail.com>
2022-09-08 15:42:04 -0300

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/components/layout/Container.jsx 	new file:   src/components/layout/Container.module.css 	new file:   src/components/layout/Footer.jsx 	new file:   src/components/layout/Footer.module.css 	new file:   src/components/layout/LinkButton.jsx 	new file:   src/components/layout/LinkButton.module.css 	new file:   src/components/layout/Navbar.jsx 	new file:   src/components/layout/Navbar.module.css 	new file:   src/components/pages/Company.jsx 	new file:   src/components/pages/Contact.jsx 	new file:   src/components/pages/Home.jsx 	new file:   src/components/pages/Home.module.css 	new file:   src/components/pages/NewProject.jsx 	new file:   src/components/pages/NewProject.module.css 	new file:   src/components/pages/Projects.jsx 	new file:   src/img/costs_logo.png 	new file:   src/img/loading.svg 	new file:   src/img/savings.svg 	new file:   src/index.js

---
+++ src/components/pages/NewProject.jsx
@@ -0,0 +1,14 @@
+
+import styles from './NewProject.module.css'
+
+function NewProject() {
+    return (
+        <div>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu pojeto para depois adicionar os serviços</p>
+            <p>Formulário</p>
+        </div>
+    )
+}
+
+export default NewProject
