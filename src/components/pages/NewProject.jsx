61f994e88a3f962b92883000e7a6a0f527a11326
Medeiros-hub <git@github.com>
2022-09-09 16:19:55 -0300

    Subindo Branch

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,12 +1,12 @@
-
+import ProjectForm from '../projects/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
     return (
-        <div>
+        <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu pojeto para depois adicionar os serviços</p>
-            <p>Formulário</p>
+            <ProjectForm />
         </div>
     )
 }
